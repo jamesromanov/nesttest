8d9a507dc6bb68270e40adda538a475fb513aa67
jamesromanov <qahramonovjamshid1@gmail.com>
2025-07-22 01:19:40 +0500

    fixed course entitty/ and dto

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,39 +1,19 @@
+import { Param } from '@nestjs/common';
+import { Prop } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class Course {
   // course title
-  @ApiProperty({
-    type: 'string',
-    default: 'Course title',
-    description: 'title of the course',
-  })
-  @IsString()
-  @IsNotEmpty()
+  @Prop()
   title: string;
   // course description
-  @ApiProperty({
-    type: 'string',
-    default: 'description of the course',
-    description: 'Course description',
-  })
-  @IsString()
-  @IsOptional()
+  @Prop()
   description: string;
   // course startdate
-  @ApiProperty({
-    type: 'string',
-    default: '2025-03-04',
-    description: 'Course created time',
-  })
-  @IsString()
+  @Prop()
   startDate: Date;
   // course enddate
-  @ApiProperty({
-    type: 'string',
-    default: '2026-09-19',
-    description: 'Course end date',
-  })
-  @IsString()
+  @Prop()
   endDate: Date;
 }
